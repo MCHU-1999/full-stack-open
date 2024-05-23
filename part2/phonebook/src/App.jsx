d49f08ee2472990eaed5dfba9a7137ad3fb7a5c8
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-23 15:21:01 +0800

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 
-const Numbers = ({ numbers }) => {
-  return 
-}
+// const Numbers = ({ numbers }) => {
+//   return 
+// }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     if (event.target.value.length >= 100) {
-      window.alert("Name shouldn't exceed 10 characters")
+      window.alert("Name shouldn't exceed 100 characters")
     } else {
       // console.log(event.target.value)
       setNewName(event.target.value)
+    }
+  }
+
+  const handleNumChange = (event) => {
+    if (event.target.value.length >= 100) {
+      window.alert("Phone number shouldn't exceed 100 characters")
+    } else {
+      // console.log(event.target.value)
+      setNewNum(event.target.value)
     }
   }
 
@@ -23,12 +33,15 @@ const App = () => {
     event.preventDefault()
     console.log('button clicked', event.target)
     if (newName.length === 0) {
-      window.alert("The 'name' cannot be empty!")
+      window.alert("Name cannot be empty!")
+    } else if (newNum.length === 0) {
+      window.alert("Number cannot be empty!")
     } else if (persons.findIndex(element => newName === element.name) >= 0) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newNum }))
       setNewName('')
+      setNewNum('')
     }
   }
 
@@ -37,7 +50,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+          <br></br>
+          number: <input value={newNum} onChange={handleNumChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -45,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        { persons.map((element) => <li key={element.name}>{ element.name }</li>) }
+        { persons.map((element) => <li key={element.name}>{ element.name } { element.number }</li>) }
       </ul>
     </div>
   )
