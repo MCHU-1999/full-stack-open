55517495455d217f8691af3726ddf963cf6e00de
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-23 15:05:05 +0800

    phonebook init && 2.6: The Phonebook Step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Numbers = ({ numbers }) => {
+  return 
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    if (event.target.value.length >= 100) {
+      window.alert("Name shouldn't exceed 10 characters")
+    } else {
+      // console.log(event.target.value)
+      setNewName(event.target.value)
+    }
+  }
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    if (newName.length === 0) {
+      window.alert("The 'name' cannot be empty!")
+    } else {
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        { persons.map((element) => <li key={element.name}>{ element.name }</li>) }
+      </ul>
+    </div>
+  )
+}
+
+export default App
