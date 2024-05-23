1e0a8d5f5cb0818e3b8eacafa447908d31eb98d8
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-23 16:11:22 +0800

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,10 +6,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [query, setQuery] = useState('')
 
   const handleNameChange = (event) => {
     if (event.target.value.length >= 100) {
@@ -29,6 +33,16 @@ const App = () => {
     }
   }
 
+  const handleSearch = (event) => setQuery(event.target.value)
+
+  const search = (list, query) => {
+    if (query === '') {
+      return list
+    } else {
+      return list.filter(element => element.name.toLowerCase().indexOf(query.toLowerCase()) > -1)
+    }
+  } 
+
   const addNumber = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
@@ -39,15 +53,20 @@ const App = () => {
     } else if (persons.findIndex(element => newName === element.name) >= 0) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName, number: newNum }))
+      setPersons(persons.concat({ name: newName, number: newNum, id: persons.length+1 }))
       setNewName('')
       setNewNum('')
     }
   }
 
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input value={query} onChange={handleSearch} />
+        </div>
+      <h2>Add a new</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -60,7 +79,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        { persons.map((element) => <li key={element.name}>{ element.name } { element.number }</li>) }
+        { search(persons, query).map(element => <li key={element.name}>{ element.name } { element.number }</li>) }
       </ul>
     </div>
   )
