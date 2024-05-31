1230523ab40df4065159bfefd2429a4317d6dc4b
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-05-31 16:15:26 +0800

    4-d update

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
 
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = process.env.TEST_MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,9 +13,8 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  /*
   const note = new Note({
-    content: 'HTML is x',
+    content: 'CSS is hard',
     important: true,
   })
 
@@ -29,11 +22,12 @@ mongoose.connect(url).then(() => {
     console.log('note saved!')
     mongoose.connection.close()
   })
-  */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+
+
+  // Note.find({}).then(result => {
+  //   result.forEach(note => {
+  //     console.log(note)
+  //   })
+  //   mongoose.connection.close()
+  // })
 })
