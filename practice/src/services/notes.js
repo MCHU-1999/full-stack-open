60fb71ddbddc6afb6c94081d3f8c048c7417919a
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-06-06 15:50:34 +0800

    5-7

--- practice/src/services/notes.js
+++ practice/src/services/notes.js
@@ -1,30 +1,36 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/notes'
+// const baseUrl = '/api/notes'
 
-/*
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}*/
 
-const getAll = () => {
+let token = null
+
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = async () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    important: true,
+  const response = await request
+  return response.data
+}
+
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
   }
-  return request.then(response => response.data.concat(nonExisting))
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+const update = async (id, newObject) => {
+  const request = axios.put(`${ baseUrl }/${id}`, newObject)
+  const response = await request
+  return response.data
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
